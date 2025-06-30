d8f09b712cd58d33a60310c53909c4234d6c70f8
krawwwwy <krakenlex@mail.ru>
2025-06-30 15:09:38 +0300

    fix log messages in config.ho

--- internal/config/config.go
+++ internal/config/config.go
@@ -31,13 +31,13 @@ func MustLoad() *Config {
 
 	//Проверяем существование конфигурационного файла
 	if _, err := os.Stat(congifPath); os.IsNotExist(err) {
-		log.Fatalf("logger file does not exist: %s", congifPath)
+		log.Fatalf("config file does not exist: %s", congifPath)
 	}
 
 	var cfg Config
 
 	if err := cleanenv.ReadConfig(congifPath, &cfg); err != nil {
-		log.Fatalf("error reading logger: %s", err)
+		log.Fatalf("error reading config: %s", err)
 	}
 
 	return &cfg
